b5c9efa37498b38e17d0201ca5b0794ce4fbeec2
nfcworkflow <wiseman@Rusted-Roof-iMac.local>
2013-10-12 20:00:45 -0400

    Lab 4- Write to Tag Example
    Adds a checkbox to the UI, and when checked, writes data to the NFC tag
    when scanned.

--- play-tag/src/com/andevcon/playtag/MainActivity.java
+++ play-tag/src/com/andevcon/playtag/MainActivity.java
@@ -1,18 +1,27 @@
 package com.andevcon.playtag;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.CheckBox;
 
 import com.andevcon.playtag.util.HexUtil;
 
 public class MainActivity extends Activity {
 
+	private static final String DEBUG_MAIN_ACTIVITY = "MainActivity";
 	private NfcAdapter nfcAdapter;
 	private PendingIntent nfcPendingIntent;
 
@@ -39,7 +48,7 @@ public class MainActivity extends Activity {
 		if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
 			Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 			String tagId = HexUtil.bytesToHex(tag.getId());
-			Log.i("MainActivity",
+			Log.i(DEBUG_MAIN_ACTIVITY,
 					String.format("Found NFC Tag with serial number %s", tagId));
 			onNfcTagDiscovered(tag, intent);
 		}
@@ -65,7 +74,38 @@ public class MainActivity extends Activity {
 	}
 
 	private void onNfcTagDiscovered(Tag tag, Intent intent) {
-		// Do something cool here
+		CheckBox cbox = (CheckBox) findViewById(R.id.checkBoxWrite);
+		if (cbox.isChecked()) {
+			writeDataToTag(tag);
+		}
+	}
+
+	private void writeDataToTag(Tag tag) {
+		String msg = "Hello Tag";
+		NdefRecord r = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+				NdefRecord.RTD_TEXT, null, msg.getBytes());
+		NdefMessage m = new NdefMessage(new NdefRecord[] { r });
+
+		Ndef ndef = Ndef.get(tag);
+		try {
+			if (null != ndef) {
+				ndef.connect();
+				ndef.writeNdefMessage(m);
+				ndef.close();
+			} else {
+				NdefFormatable ndefFormat = NdefFormatable.get(tag);
+				ndefFormat.connect();
+				ndefFormat.format(m);
+				ndefFormat.close();
+			}
+
+			Log.i(DEBUG_MAIN_ACTIVITY, "Text successfully written to tag");
+
+		} catch (IOException e) {
+			Log.e(DEBUG_MAIN_ACTIVITY, e.getMessage());
+		} catch (FormatException e) {
+			Log.e(DEBUG_MAIN_ACTIVITY, "Invalid Format");
+		}
 	}
 
 	@Override
